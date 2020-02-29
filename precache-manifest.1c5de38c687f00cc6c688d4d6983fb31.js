54bce7a9413ec0bc3c74729bd55bdab4706a1050
sandra <sandra.h.arriaga@gmail.com>
2020-02-28 20:51:32 -0600

    Updates

---
+++ precache-manifest.1c5de38c687f00cc6c688d4d6983fb31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c102ea92f15fb4d46b4d4500cd3976f7",
+    "url": "/Kualli/info.git/index.html"
+  },
+  {
+    "revision": "02a9c2a6b7b1ac589f1d",
+    "url": "/Kualli/info.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4b54ca6ac1334a5b64d8",
+    "url": "/Kualli/info.git/static/js/2.3ba1be89.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Kualli/info.git/static/js/2.3ba1be89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02a9c2a6b7b1ac589f1d",
+    "url": "/Kualli/info.git/static/js/main.7350c18f.chunk.js"
+  },
+  {
+    "revision": "a71c4dcf24197cc24847",
+    "url": "/Kualli/info.git/static/js/runtime-main.6521e2c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Kualli/info.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
